bba41b036e2b9ab0458a551d4739fa057af7032c
vbmakarov <vbmakarov.info@gmail.com>
2021-05-12 22:28:37 +0300

    3.3. Разбор первой программы. [#159033 #195356]

---
+++ src/ru/job4j/StudentInfo.java
@@ -0,0 +1,15 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args){
+        System.out.println("1х1=1");
+        System.out.println("1х2=2");
+        System.out.println("1х3=3");
+        System.out.println("1х4=4");
+        System.out.println("1х5=5");
+        System.out.println("1х6=6");
+        System.out.println("1х7=7");
+        System.out.println("1х8=8");
+        System.out.println("1х9=9");
+    }
+}
